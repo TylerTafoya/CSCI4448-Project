53473ccbc3f16691163a254cc85e412efd455548
Mitchtz <mitchtz@gmail.com>
2015-12-01 18:23:51 -0700

    Piece creation for both players complete

--- ChessXpress/src/Game.java
+++ ChessXpress/src/Game.java
@@ -9,9 +9,45 @@ public class Game{
 		Piece[] player1 = new Piece[16];
 		//List of player 1's pieces
 		Piece[] player2 = new Piece[16];
+		//Player 1 piece creation
+		//Create pawns for player1
+		for (int i=0; i<8; i++)
+		{
+			player1[i] = new Pawn(true,1,i);
+		}
 		//Create rooks for player1
-		player1[0] = new Rook(true,0,0);
-		player1[1] = new Rook(true,0,7);
+		player1[8] = new Rook(true,0,0);
+		player1[9] = new Rook(true,0,7);
+		//Create knights for player1
+		player1[10] = new Knight(true,0,1);
+		player1[11] = new Knight(true,0,6);
+		//Create bishops for player1
+		player1[12] = new Bishop(true,0,2);
+		player1[13] = new Bishop(true,0,5);
+		//Create king for player1
+		player1[14] = new King(true,0,3);
+		//Create queen for player 1
+		player1[15] = new Queen(true,0,4);
+		
+		//Player 2 piece creation
+		//Create pawns for player1
+		for (int i=0; i<8; i++)
+		{
+			player2[i] = new Pawn(true,6,i);
+		}
+		//Create rooks for player2
+		player2[8] = new Rook(true,7,0);
+		player2[9] = new Rook(true,7,7);
+		//Create knights for player2
+		player2[10] = new Knight(true,7,1);
+		player2[11] = new Knight(true,7,6);
+		//Create bishops for player2
+		player2[12] = new Bishop(true,7,2);
+		player2[13] = new Bishop(true,7,5);
+		//Create king for player2
+		player2[14] = new King(true,7,3);
+		//Create queen for player 1
+		player2[15] = new Queen(true,7,4);
 
 	}
 
