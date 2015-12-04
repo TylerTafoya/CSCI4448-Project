1cdfadfaafb3e12bdf3fd9dce450be0d9deff64e
Mitchtz <mitchtz@gmail.com>
2015-12-03 18:51:53 -0700

    Rook isValid should work

--- ChessXpress/src/Rook.java
+++ ChessXpress/src/Rook.java
@@ -6,14 +6,110 @@ public class Rook extends Piece {
 	
 	@Override
 	public boolean isValid(Piece[][] board, int destX, int destY) {
+		//Check if it's on the board
 		if(super.isValid(board, destX, destY) == false)
 			return false;
 		//Rooks move horizontal or vertical
+		//Check vertical case
 		if(destX == this.getX())
-			return true;
-		
+			//Destination is above the piece
+			if (destX < this.getX()) {
+				//Iterate backwards towards destination
+				for (int i=this.getX()-1; i>=destX; i--) {
+					//Check if the spot is your destination
+					if (i == destX) {
+						//Return false if it is your own piece, otherwise true
+						if (board[i][this.getY()].getPlayer() == (1-this.getPlayer())) {
+							return false;
+						}
+						//Otherwise it's valid
+						else {
+							return true;
+						}
+					}
+					//Otherwise spot is not destination
+					else {
+						//Return false is space is occupied (not Blank
+						if (!(board[i][this.getY()].getPlayer() == 2)) {
+							return false;
+						}
+					}
+				}
+			}
+			//Destination is below the piece
+			else {
+				//Iterate forwards towards destination
+				for (int i=this.getX()+1; i<=destX; i++) {
+					//Check if the spot is your destination
+					if (i == destX) {
+						//Return false if it is your own piece, otherwise true
+						if (board[i][this.getY()].getPlayer() == (1-this.getPlayer())) {
+							return false;
+						}
+						//Otherwise it's valid
+						else {
+							return true;
+						}
+					}
+					//Otherwise spot is not destination
+					else {
+						//Return false is space is occupied (not Blank
+						if (!(board[i][this.getY()].getPlayer() == 2)) {
+							return false;
+						}
+					}
+				}
+		}
+		//Horizontal case
 		if(destY == this.getY())
-			return true;
+			//Destination is to the left of the piece
+			if (destY < this.getY()) {
+				//Iterate backwards towards destination
+				for (int i=this.getY()-1; i>=destY; i--) {
+					//Check if the spot is your destination
+					if (i == destY) {
+						//Return false if it is your own piece, otherwise true
+						if (board[this.getX()][i].getPlayer() == (1-this.getPlayer())) {
+							return false;
+						}
+						//Otherwise it's valid
+						else {
+							return true;
+						}
+					}
+					//Otherwise spot is not destination
+					else {
+						//Return false is space is occupied (not Blank
+						if (!(board[this.getX()][i].getPlayer() == 2)) {
+							return false;
+						}
+					}
+				}
+			}
+			//Destination is to the right of the piece
+			else {
+				//Iterate towards destination
+				for (int i=this.getY()+1; i<=destY; i++) {
+					//Check if the spot is your destination
+					if (i == destY) {
+						//Return false if it is your own piece, otherwise true
+						if (board[this.getX()][i].getPlayer() == (1-this.getPlayer())) {
+							return false;
+						}
+						//Otherwise it's valid
+						else {
+							return true;
+						}
+					}
+					//Otherwise spot is not destination
+					else {
+						//Return false is space is occupied (not Blank
+						if (!(board[this.getX()][i].getPlayer() == 2)) {
+							return false;
+						}
+					}
+				}
+		}
 		
 		return false;
 	}
